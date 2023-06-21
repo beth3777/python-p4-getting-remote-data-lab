d355d83b5ef7a6390e142b0146289fd0abfb25a6
email <bethkimaku@gmail.com>
2023-06-21 15:14:26 +0300

    completed

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,18 @@
 import requests
 import json
 
+url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+            response = requests.get(url)
+            return response.content
 
     def load_json(self):
-        pass
+         response_body= self.get_response_body()
+         data = json.loads(response_body)
+         return data
+    
+
